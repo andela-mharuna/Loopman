eb26ab15b6c619cf2cd4a8eaab80667f3c1747aa
memuna haruna <memuna.haruna@andela.com>
2017-05-15 18:42:26 +0100

    Fix heroku build issue: not finding .env file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
 const webpack = require('webpack');
 const dotenv = require('dotenv').config();
-console.log(dotenv);
-const env = dotenv.parsed;
-
-console.log(env, JSON.stringify(env.API_KEY));
 
 module.exports = {
   devtool: 'eval-source-map',
@@ -20,7 +16,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process': {
         'env': {
-          'API_KEY': JSON.stringify(env.API_KEY)
+          'API_KEY': JSON.stringify(process.env.API_KEY)
         }
       }
     })
